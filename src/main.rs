abbb43bf68eb38cc38dd24aaecb7b512b1f0473b
Kai Schmidt <kaikaliischmidt@gmail.com>
2023-04-25 10:06:28 -0700

    don't use UI for now

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,35 @@ mod node;
 mod parse;
 mod ui;
 
-use std::ops::{Add, Rem};
+use std::{
+    fs,
+    ops::{Add, Rem},
+};
 
-use ui::App;
+use hodaun::{Mix, OutputDeviceMixer, Stereo};
+
+use crate::compile::compile;
 
 fn main() {
-    let mut app = App::default();
-    app.load("test.cube");
-    eframe::run_native(
-        "Cube",
-        eframe::NativeOptions {
-            ..Default::default()
-        },
-        Box::new(|cc| {
-            cc.egui_ctx.set_pixels_per_point(3.0);
-            Box::new(app)
-        }),
-    )
-    .unwrap();
+    let input = match fs::read_to_string("test.cube") {
+        Ok(input) => input,
+        Err(e) => {
+            println!("{e}");
+            return;
+        }
+    };
+    let source = match compile(&input) {
+        Ok(source) => source,
+        Err(e) => {
+            println!("{e}");
+            return;
+        }
+    };
+    let mut output = OutputDeviceMixer::<Stereo>::with_default_device().unwrap();
+    if let Some(source) = source {
+        output.add(source);
+    }
+    output.play_blocking().unwrap();
 }
 
 pub fn modulus<T, M>(a: T, m: M) -> <<<T as Rem<M>>::Output as Add<M>>::Output as Rem<M>>::Output
